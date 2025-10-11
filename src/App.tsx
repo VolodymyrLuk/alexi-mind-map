0f872f32b34477ab6d58354458903fee3585a2cf
Volodymyr Luk <105209391+VolodymyrLuk@users.noreply.github.com>
2025-10-11 16:54:45 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/alexi-mind-map">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
